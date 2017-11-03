9152020dc55f958a458034f860e8980e5d9a3ee3
scadapy <32699436+scadapy@users.noreply.github.com>
2017-11-03 23:12:36 +0300

    Delete start_71.bat

--- scr/start_71.bat
+++
@@ -1,9 +0,0 @@
-rem Скрипт создан в программе 'ScadaPy Конфигуратор сервера v.3.10'
-rem Название сервера 'Windows opcua_master_tcp'
-rem Slave адрес '192.168.0.101'
-rem Slave порт '4840'
-rem Тип master 'master_modbusTCP'
-rem Тип slave 'opcUA'
-rem Интерфейс tty ''
-rem Скорость tty ''
-start c:\Python35\python.exe F:\scadapy\config\source\opcua_master_tcp.py 71 F:\scadapy\config\db\srvDb.db
