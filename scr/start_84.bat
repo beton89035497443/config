77c86311620885287c10301f8816061f6b2028ef
scadapy <32699436+scadapy@users.noreply.github.com>
2017-11-03 23:06:47 +0300

    Add files via upload

---
+++ scr/start_84.bat
@@ -0,0 +1,9 @@
+rem Скрипт создан в программе 'ScadaPy Конфигуратор сервера v.3.11'
+rem Название сервера 'Windows modbus_master_rtu'
+rem Slave адрес '192.168.0.103'
+rem Slave порт '502'
+rem Тип master 'master_modbusRTU'
+rem Тип slave 'modbusTCP'
+rem Интерфейс tty 'COM6'
+rem Скорость tty '9600'
+start c:\Python35\python.exe F:\scadapy\config\source\modbustcp_master_rtu.py 84 F:\scadapy\config\db\srvDb.db
---
+++ scr/start_91.bat
@@ -0,0 +1,9 @@
+rem Скрипт создан в программе 'ScadaPy Конфигуратор сервера v.3.11'
+rem Название сервера 'Windows opcua_master_rtu'
+rem Slave адрес '192.168.0.103'
+rem Slave порт '4840'
+rem Тип master 'master_modbusRTU'
+rem Тип slave 'opcUA'
+rem Интерфейс tty 'COM6'
+rem Скорость tty '9600'
+start c:\Python35\python.exe F:\scadapy\config\source\opcua_master_rtu.py 91 F:\scadapy\config\db\srvDb.db
